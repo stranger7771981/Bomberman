b6130ff386f482b220e07625515f2cd8773598cd
Denis Uglov <denisuglov1981@gmail.com>
2019-12-13 12:55:35 +0600

    first commit

---
+++ Assets/Scripts/DestroySelf.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class DestroySelf : MonoBehaviour
+{
+    public float Delay = 3f;
+    //Delay in seconds before destroying the gameobject
+
+    void Start ()
+    {
+        Destroy (gameObject, Delay);
+    }
+}
---
+++ Assets/Scripts/GlobalStateManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlobalStateManager : MonoBehaviour
+{
+    private int deadPlayers = 0;
+    private int deadPlayersNumber = -1;
+
+    public void PlayerDied (int playerNumber)
+    {
+        deadPlayers++;
+        if (deadPlayers == 1)
+        {
+            deadPlayersNumber = playerNumber;
+            Invoke("CheckPlayersDeath", .3f);
+        }
+
+    }
+
+    void CheckPlayersDeath()
+    {
+        // 1
+        if (deadPlayers == 1)
+        {
+            // 2
+            if (deadPlayersNumber == 1)
+            {
+                Debug.Log("Player 2 is the winner!");
+                // 3
+            }
+            else
+            {
+                Debug.Log("Player 1 is the winner!");
+            }
+            // 4
+        }
+        else
+        {
+            Debug.Log("The game ended in a draw!");
+        }
+    }
+}
